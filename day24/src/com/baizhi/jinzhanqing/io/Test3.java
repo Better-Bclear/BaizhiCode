19b6f3add4d27ba0d233297784ae506508dada0f
jin1357908642@163.com <jin1357908642@163.com>
2023-05-29 10:55:43 +0800

    上课练习提交

--- day24/src/com/baizhi/jinzhanqing/io/Test3.java
+++ day24/src/com/baizhi/jinzhanqing/io/Test3.java
@@ -9,19 +9,25 @@ import java.io.IOException;
 * */
 public class Test3 {
     public static void main(String[] args) {
+        //1.在trycatch外定义流变量
         FileOutputStream fileOutputStream = null;
         try {
+            //2.在try中赋值
             fileOutputStream = new FileOutputStream("hello.txt");
+            //3.写入文件中
             fileOutputStream.write("helloworld".getBytes());
         }catch (IOException e){
             e.printStackTrace();
         }finally {
+            //3.判断流对象是否为空
             if (fileOutputStream != null){
                 try {
+                    //4.关闭资源
                     fileOutputStream.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }finally {
+                    //5.无论是否close成功，在finally中将对象设置为null
                     fileOutputStream = null;
                 }
             }
